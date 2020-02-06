8c6bdbf601e351540f0c85f74945489ab2c6b353
Jaren Taylor <jaren4622@gmail.com>
2020-02-05 23:18:50 -0500

    "Added checks for the diagonal win boxes as well as checks for if a spot was taken"

--- Connect Four/connectFour.js
+++ Connect Four/connectFour.js
@@ -1,5 +1,6 @@
 let players = ['red','black'];
 let currPlayer;
+let playing;
 
 //we draw the board on a 500x500 space, and for connect four you need to have 6 columns and 7 rows, so thats 500/6 and 500/7.
 let xinc = 500/7; 
@@ -20,7 +21,8 @@ let board = [
 
 function setup() {
 createCanvas(1000,1000);
-currPlayer = players[ceil(random())];
+playing = true;
+currPlayer = players[round(Math.random())];
 }
 
 function turnHandler(){
@@ -34,10 +36,9 @@ function turnHandler(){
     }
   }
 }
+
 function checkFour(a,b,c,d){
-
 return ( ((a == b) && (b == c) && (c == d)) && (a != '') );
-
 }
 
 function outcome(){
@@ -46,41 +47,83 @@ function outcome(){
   for(let i = 0; i <= 3; i++){
     if(checkFour(board[j][i],board[j][i+1],board[j][i+2],board[j][i+3])){
       console.log(board);
-      console.log(currPlayer + " wins.")
+      playing = false;
     }
   }
 }
 
-  //vertical check
-//   for(let j = 0; j <= 2; j++){
-//     for(let i=0; i< 7; i++){
-//     if(checkFour(board[i][j],board[i][j+1],board[i][j+2],board[i][j+3])){
-//     console.log(currPlayer + " wins.")
-//     }
-//   }
-//   //diagonal check
-  
-// }
- }
+  // vertical check
+  for(let j = 0; j <= 2; j++){
+    for(let i=0; i< 7; i++){
+    if(checkFour(board[j][i],board[j+1][i],board[j+2][i],board[j+3][i])){
+    console.log(currPlayer + " wins.")
+    playing = false;
+    }
+  }
+}
 
+// diagonal check(Bottom-Backwards)
+for(let i = 6; i >= 3; i--){
+for(let j = 5; j >= 3; j--){
+  if(checkFour(board[j][i],board[j-1][i-1],board[j-2][i-2],board[j-3][i-3])){
+    console.log(currPlayer + " wins.");
+    playing = false;
+    return;
+  }
+}
+}
+
+//diagonal check (Bottom-forwards)
+for(let i = 0; i <= 3; i++){
+  for(let j = 5; j >= 3; j--){
+    if(checkFour(board[j][0+i],board[j-1][1+i],board[j-2][2+i],board[j-3][3+i])){
+      console.log(currPlayer + " wins.");
+      playing = false;
+      return;
+    }
+  }
+  }
+}
 // now we want to draw the circles in the corresponding spots if they are clicked by a player.
 function mousePressed(){
   // alert(currPlayer+ "'s turn"); yeah this is kind of annoying so I turned it off.
+  if(playing == true){
   if((mouseX >= 0 && mouseX <= 500) && ((mouseY >= 0 && mouseY <= 500))){
   
 for(let j = 0; j <= 5 ; j++){
   for(let i = 0; i <= 6 ; i++){
-      if( ((mouseX >= xinc*i) && (mouseX <= xinc *(i+1)) ) && ((mouseY >= yinc*j) && (mouseY <= yinc * (j+1)) ) ){
+      if( ((mouseX >= xinc*i) && (mouseX <= xinc *(i+1)) ) 
+      && ((mouseY >= yinc*j) && (mouseY <= yinc * (j+1)) ) ){
+        if(board[j][i] == ''){
         fill(currPlayer);
         circle((xinc/2)*((2*i)+1),(yinc/2)*((2*j)+1),r);
         noFill();
         board[j][i] = currPlayer;
         outcome();
         turnHandler();
+        }
+        else{
+          alert("Spot already taken.");
+        }
+      }
       }
     }
   }
 }
+}
+
+function resetBoard(){
+   board = [
+    ['','','','','','',''],
+    ['','','','','','',''],
+    ['','','','','','',''],
+    ['','','','','','',''],
+    ['','','','','','',''],
+    ['','','','','','',''],
+    
+    ];
+    playing = true;
+    redraw(); // we redraw the board once, so that we can have a fresh game.
 }
 
 function draw() {
